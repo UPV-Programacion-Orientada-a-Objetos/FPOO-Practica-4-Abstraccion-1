8e1f30a9b7aa57184a88b31be97edc052c494b81
Israel <2230082@upv.edu.mx>
2024-02-26 13:03:18 -0600

    prestamo 1.0

--- herenciaAbstracta1/src/main/java/edu/upvictoria/fpoo/Prestamos.java
+++ herenciaAbstracta1/src/main/java/edu/upvictoria/fpoo/Prestamos.java
@@ -1,5 +1,9 @@
 package edu.upvictoria.fpoo;
+import edu.upvictoria.fpoo.Usuarios.Administrador;
 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 public class Prestamos {
     public int id;
     public String Usuario;
@@ -9,6 +13,22 @@ public class Prestamos {
     public String fechaInic;
     public String fechaFin;
     public String estado;
-    
+
+    public void prestamo()throws IOException{
+        Menu mn=new Menu();
+        String tt= mn.inicio_Sesion();
+        if(tt!="No existe"){
+            if(tt.equals("Profesor")&&tt.equals("Estudiante")){
+                System.out.println("Usuario: "+tt);
+                Administrador admin=new Administrador();
+                admin.FuncionesAdmin();
+            }else{
+                System.out.println("Acceso Denegado\nIntente nuevamente");
+            }
+        }else if(tt.equals("No existe")){
+            System.out.println("Intente nuevamente");
+        }
+
+    }
 
 }
